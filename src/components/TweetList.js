dd631c1afacbcea05ea98b94322de1a1b89ba8c5
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-02-07 08:40:31 -0800

    Update TweetList.js

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import TweetItem from "./TweetItem";
 
+
 function TweetList(props) {
+console.log(props.handleTweetLikes)
   return (
     <div className="ui segment">
       <div className="ui feed">
         {props.user.tweets.map((tweet) => (
           <TweetItem
             key={tweet.id}
-            handleTweetLike={props.handleTweetLike}
+            handleTweetLikes={props.handleTweetLikes}
             handle={props.user.handle}
             photo={props.user.photo}
             tweet={tweet}
