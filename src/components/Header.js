3dd26500538d6ee85f960e7a468624a17fb1905c
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-02-07 08:39:36 -0800

    Update Header.js
    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Menu from "./Menu";
 
-function Header() {
-  const [darkMode, setDarkMode] = useState(false);
+function Header({setDarkMode, darkMode}) {
+ 
 
   return (
     <div className={`ui fixed menu ${darkMode ? "inverted" : ""}`}>
       <div className="menu item">Fwitter</div>
       <div className="right menu">
-        <Menu />
+        <Menu setDarkMode={setDarkMode} darkMode={darkMode} />
       </div>
     </div>
   );
